cb3de8334e862589ac3c8488ff26a33983601be5
liyengwas <liyengwas@gmail.com>
2020-05-01 07:45:33 +0300

    Product Styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+   <section class="products">
+    
+     <Product 
+      v-for="product in products"
+      :key="product.color"
+      :product="product"
+     />
+
+   </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Product from './components/Product';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
+  },
+
+  data () {
+    return {
+      products : [
+        {
+          title: 'Nissan R32',
+          color: 'black',
+          bgtext: 'R32',
+          src: require('./assets/r32.jpg') 
+        },
+        {
+          title: 'Nissan R33',
+          color: 'black',
+          bgtext: 'R33',
+          src: require('./assets/r33.jpg') 
+        },
+        {
+          title: 'Nissan R34',
+          color: 'blue',
+          bgtext: 'R34',
+          src: require('./assets/r34.jpg') 
+        }
+        // {
+        //   title: 'Nissan R35',
+        //   color: 'white',
+        //   bgtext: 'R35',
+        //   src: require('./assets/r35.jpg') 
+        // }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main{
+    width: 100vw;
+    min-height: 100vh;
+    overflow: hidden;
+
+    background-color: #EEE;
+    display: flex;
+    align-items: center;
+  }
+
+  .products{
+    display: flex;
+    max-width: 1280px;
+    padding: 25px;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="product">
+        <div class="product-inner">
+            <div class="product-text-wrap">
+                <h2 class="bg-text">
+                    {{ product.bgtext }}
+                </h2>
+            </div>
+            <div class="product-image-wrap">
+                <img :src="product.src" class="image" />
+            </div>
+            <div class="product-detail">
+                <h2> {{ product.title }} </h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore totam maxime nisi nulla consequatur nihil tenetur doloremque obcaecati hic atque maiores dolores ullam, sint non neque veritatis iusto animi laborum.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'product',
+    props: ['product']
+}
+</script>
+
+<style>
+    
+    .product{
+        flex: 1 1 33.333%;
+        width: 100%;
+        padding: 25px;
+    }
+
+    .product-inner{
+        position: relative;
+        padding: 25px;
+        box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    }
+
+    .product-image-wrap .image{
+        width: 100%;
+    }
+
+</style>
